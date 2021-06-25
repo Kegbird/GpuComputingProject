ca03c4d1dbcee1697a64543f9dd2862ba3df0f8c
Kegbird <lazyduckdev@gmail.com>
2021-06-25 12:25:37 +0200

    Added visual studio project and stbi library.

---
+++ GpuComputingProject/main.cu
@@ -0,0 +1,34 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#define STB_IMAGE_IMPLEMENTATION 
+#include <stb_image.h>
+
+int main()
+{
+	int width;
+	int height;
+	int channels;
+	size_t image_size;
+	unsigned char* image = stbi_load("prova.png", &width, &height, &channels, 0);
+	if (image == NULL)
+	{
+		printf("No image provided!");
+		return 0;
+	}
+	image_size = width * height * channels;
+
+	printf("Number of channels:%d\n", channels);
+	for (unsigned char* pixel = image; pixel != image + image_size; pixel+=channels)
+	{
+		printf("%d ",(int)pixel[0]);
+		printf("%d ", (int)pixel[1]);
+		printf("%d ", (int)pixel[2]);
+		printf("\n");
+	}
+
+	free(image);
+}
+
